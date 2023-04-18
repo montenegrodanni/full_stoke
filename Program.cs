62859e8c90c2193a130071ffbe95034fba221f69
montenegrodani <montenegro10.daniel@gmail.com>
2023-04-18 14:40:59 -0300

    acrescimo de linhas

--- Program.cs
+++ Program.cs
@@ -1,2 +1,52 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace full_stoke;
+
+
+class Program
+{
+
+    public static void Main(string[] args)
+    {
+
+        int n1 = 0, n2 = 0, n3 = 0, media = 0;
+        char parabens = (char)0;
+
+
+        Console.WriteLine("exame nacional do dani, prova final");
+        Console.Write("Digite a primeira nota: ");
+        n1 = int.Parse(Console.ReadLine());
+        Console.Write("Digite a segunda nota: ");
+        n2 = int.Parse(Console.ReadLine());
+        Console.Write("Digite a terceira nota: ");
+        n3 = int.Parse(Console.ReadLine());
+
+        media = (n1 + n2) / 2;
+
+        if (media > 60)
+        {
+            Console.WriteLine(" voce Passou!");
+        }
+        else if (media == 60)
+        {
+            Console.WriteLine("Na média!");
+
+
+        }
+        if (media >= 80)
+
+            Console.WriteLine(" muito bem ");
+
+        else
+        {
+            Console.WriteLine("Rodou!");
+        }
+
+    }
+
+    private static void readline(string v)
+    {
+        throw new NotImplementedException();
+    }
+
+
+
+}
